f61cf1954a70340558c3d903f41848adbab3cc5d
DominusMors <johnpets92@gmail.com>
2023-04-07 17:14:18 +0300

    Fixes
    Fixes on some files and implementing a modal popup for a page

--- js/custom.js
+++ js/custom.js
@@ -4,76 +4,8 @@
 
 $(function () {
    
-    // animate on scroll (initilize wow plugin
+    // animate on scroll (initilize wow plugin)
     new WOW().init();
-});
-
-/*=============================================
-                WORK
-==============================================*/
-
-// $(function (){
-    
-//     // Select HTML element with id=work    
-//     $("#work").magnificPopup({      
-//         //child item selector, by clicking on it, popup will open
-//         delegate: 'a', 
-//         type: 'image',       
-//         gallery:{
-//             enabled: true
-//         }
-//     });
-
-// });
-    
-/*=============================================
-                TEAM
-==============================================*/
-/*
-$(function() {    
-    $("#team-members").owlCarousel({        
-        items: 3,
-        autoplay: true,
-        smartSpeed: 700,             // move slower when changing
-        loop: true,
-        autoplayHoverPause: true,
-         responsive : 
-        {
-            // breakpoint from 0 up
-            0 : {
-
-                items: 1
-            },
-            // breakpoint from 480 up
-            480 : {
-
-                items: 2
-            },
-            // breakpoint from 768 up
-            768 : {
-
-                items: 3
-
-            }            
-        }
-    });
-    
-});
-*/
-/*=============================================
-                TESTIMONIALS
-==============================================*/
-$(function() {
-    
-    $("#customers-testimonials").owlCarousel({
-        
-        items: 1,
-        autoplay: true,
-        smartSpeed: 700,             // move slower when changing
-        loop: true,
-        autoplayHoverPause: true
-    });
-    
 });
 
 /*=============================================
@@ -101,7 +33,9 @@ $(function() {
 
 });
 
-//Smooth Scrolling
+/*=============================================
+                SMOOTH SCROLLING
+==============================================*/
 
 $(function() {
     
@@ -154,3 +88,66 @@ $(document).ready(function(){
     $("#myModal").modal();  
   });
   });//EOF Document.ready
+
+
+/*==================================================================================
+                                Roxy Effect
+==================================================================================*/
+
+
+// This Script makes the images on index_sights popup 
+
+function showImage(src) {
+  var modal = document.getElementById("image-modal");
+  var modalContent = document.getElementById("image-modal-content");
+  modalContent.src = src;
+  modal.style.display = "block";
+  
+  
+  var closeModal = document.getElementsByClassName("close-modal")[0];
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+    const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+    const body = document.body;
+    body.style.position = 'fixed';
+    body.style.top = `-${scrollY}`;
+    var blurBackground = document.getElementById("blur-background");
+    blurBackground.parentNode.removeChild(blurBackground);
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+        
+      var blurBackground = document.getElementById("blur-background");
+      blurBackground.parentNode.removeChild(blurBackground);
+    }
+  }
+}
+const showDialog = () => {
+    document.getElementById('image-modal').classList.add('image-modal-content')
+    const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+    const body = document.body;
+    body.style.position = 'fixed';
+    body.style.top = `-${scrollY}`;
+  };
+  const closeDialog = () => {
+    const body = document.body;
+    const scrollY = body.style.top;
+    body.style.position = '';
+    body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    document.getElementById('image-modal').classList.remove('image-modal-content');
+  }
+  window.addEventListener('scroll', () => {
+    document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+  });$
+
+
+
+/*
+ onclick="openImage('img/sights/blue_marble.jpg')"
+<script>
+function openImage(imageUrl) {
+window.open(imageUrl, '_blank', 'height=750,width=800');
+} */
